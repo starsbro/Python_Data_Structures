f8fe3c36b4a6abf452e291fdfae73696d1898669
Xingxing Xiao <xiao.xingx@northeastern.edu>
2025-01-25 18:44:07 -0500

    Update binary_search_tree file

---
+++ 2022数据结构与算法B/hashing/binary_search_tree.py
@@ -0,0 +1,85 @@
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+    
+    def length(self):
+        return self.size
+    
+    def __len__(self):
+        return self.size
+    
+    def __iter__(self):
+        return self.root.__iter__()
+
+class TreeNode:
+    def __init__(self, key,val, left = None, \
+                 right = None, parent = None):
+        self.key = key
+        self.payload = val
+        self.leftChild = left
+        self.rightChid = right
+        self.parent = parent
+
+    def hasLeftChild(self):
+        return self.leftChild
+    
+    def hasRightChild(self):
+        return self.rightChid
+    
+    def isLeftChild(self):
+        return self.parent and self.parent.leftChild == self
+    
+    def isRightChild(self):
+        return self.parent and self.parent.rightChild == self
+    
+    def __iter__(self):
+        if self:
+            if self.hasLeftChild():
+                for elem in self.leftChild:
+                    yield elem
+            yield self.key
+            if self.hasRightChild():
+                for elem in self.rightChid:
+                    yield elem
+    
+    def findSuccessor(self):
+        succ = None
+        if self.hasRightChild():
+            succ = self.rightChid.findMin()
+        else:
+            if self.parent:
+                if self.isLeftChild():
+                    succ = self.parent
+                else:
+                    self.parent.rightChild = None
+                    succ = self.parent.findSuccessor()
+                    self.parent.rightChild = self
+        return succ
+    
+    def findMin(self):
+        current = self
+        while current.hasLeftChild():
+            current = current.leftChild
+        return current
+    
+    def spliceOut(self):
+        if self.isLeaf():
+            if self.isLeftChild():
+                self.parent.leftChild = None
+            else:
+                self.parent.rightChild = None
+        elif self.hasAnyChildren():
+            if self.hasLeftChild():
+                if self.isLeftChild():
+                    self.parent.leftChild = self.leftChild
+                else:
+                    self.parent.rightChild = self.leftChild
+                self.leftChild.parent = self.parent
+            else:
+                if self.isLeftChild():
+                    self.parent.leftChild = self.rightChild
+                else:
+                    self.parent.rightChild = self.rightChild
+                self.rightChild.parent = self.parent
+
