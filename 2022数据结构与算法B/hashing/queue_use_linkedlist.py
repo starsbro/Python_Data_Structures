0fab9e1d3b966e0e21c83a89941ba95ed168a056
Xingxing Xiao <xiao.xingx@northeastern.edu>
2025-01-25 23:46:18 -0500

    Update queue code

---
+++ 2022数据结构与算法B/hashing/queue_use_linkedlist.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.front = self.rear = None
+
+    def enqueue(self, data):
+        new_node = Node(data)
+        if self.rear is None:
+            self.front = self.rear = new_node
+            return
+        self.rear.next = new_node
+        self.rear = new_node
+    
+    def dequeue(self):
+        if self.front is None:
+            return None
+        temp = self.front
+        self.front = self.front.next
+        if self.front is None:
+            self.rear = None
+        return temp.data
+
+#Example Usage
+q = Queue()
+q.enqueue(10)
+q.enqueue(20)
+print(q.dequeue()) # FIFO
+print(q.dequeue())
